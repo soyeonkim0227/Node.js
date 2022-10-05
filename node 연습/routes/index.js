83a887b376d9a7f8a52cf82085a19e0f26e656df
soyeonkim0227 <soyeonkim0227@naver.com>
2022-10-05 22:59:00 +0900

    Update node index routes

--- node 연습/routes/index.js
+++ node 연습/routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
 const router = express();
 
 const Post = require("./post");
 const User = require("./user");
+const Comment = require("./comment");
 
 router.use("/user", User);
 router.use("/post", Post);
+router.use("/post", Comment);
 
 module.exports = router;
