e7f893d9ef0fe3b5b636a966d0461b2c148fb989
soyeonkim0227 <soyeonkim0227@naver.com>
2022-10-20 13:40:31 +0900

    Update node like api

--- node 연습/models/like.js
+++ node 연습/models/like.js
@@ -2,6 +2,11 @@ const sequelize = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define("like", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
         user_id: {
             type: DataTypes.INTEGER,
             allowNull: false
