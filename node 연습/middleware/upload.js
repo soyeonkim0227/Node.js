25402916d56722dc94aa5a87705cebce680668a1
soyeonkim0227 <soyeonkim0227@naver.com>
2022-11-08 20:58:27 +0900

    Create node upload middleware

---
+++ node 연습/middleware/upload.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+const path = require("path");
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, "uploads/");
+        },
+        filename: (req, file, cb) => {
+            cb(null, new Date().valueOf() + path.extname(file.originalname));
+        }
+    })
+});
+
+module.exports = upload; 
