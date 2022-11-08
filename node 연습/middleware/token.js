b74f64fb0707fb52c35dcdd648f25047ecb1e165
soyeonkim0227 <soyeonkim0227@naver.com>
2022-11-08 20:55:45 +0900

    Update node token

--- node 연습/middleware/token.js
+++ node 연습/middleware/token.js
@@ -4,7 +4,7 @@ const tokenMiddleware = async(req, res, next) => {
     const token = await req.headers["access-token"]; // 포스트맨에 이 이름을 넣어서 갖고오기 위한
 
     if (!token) {
-        res.status(401).json({ // 토큰 있는지 확인
+        return res.status(401).json({ // 토큰 있는지 확인
             message: "로그인 되지 않음"
         });
     }
@@ -16,10 +16,11 @@ const tokenMiddleware = async(req, res, next) => {
             next();
         });
     } catch(err) {
-        res.status(401).json({
+        console.error(err);
+
+        return res.status(401).json({
             message: "로그인 되어 있지 않음"
         });
-        console.error(err);
     }
 };
 
